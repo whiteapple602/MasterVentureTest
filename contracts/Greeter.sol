6ff66ba2ac697a60833e535b15ea836a2b1c6361
Ryan Carter <whiteapple8222@gmail.com>
2021-12-02 23:19:29 -0500

    write the basci contract and the test of it

--- contracts/Greeter.sol
+++ contracts/Greeter.sol
@@ -1,22 +1,10 @@
 //SPDX-License-Identifier: Unlicense
 pragma solidity ^0.8.0;
-
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "hardhat/console.sol";
 
-contract Greeter {
-    string private greeting;
-
-    constructor(string memory _greeting) {
-        console.log("Deploying a Greeter with greeting:", _greeting);
-        greeting = _greeting;
-    }
-
-    function greet() public view returns (string memory) {
-        return greeting;
-    }
-
-    function setGreeting(string memory _greeting) public {
-        console.log("Changing greeting from '%s' to '%s'", greeting, _greeting);
-        greeting = _greeting;
+contract Greeter is ERC20 {
+    constructor(uint256 initialBalance) public ERC20("Basic", "BSC") {
+        _mint(msg.sender, initialBalance);
     }
 }
