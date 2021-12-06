b77339bad1d5280a8a96f503ee9f2f643bc8aec1
Ryan Carter <whiteapple8222@gmail.com>
2021-12-06 12:12:38 -0500

    make stake function working

--- contracts/Staking.sol
+++ contracts/Staking.sol
@@ -2,6 +2,7 @@
 pragma solidity ^0.8.4;
 import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
 import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/token/ERC20/IERC20Upgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/security/PausableUpgradeable.sol";
 
 contract SimpleStaking is
@@ -10,9 +11,12 @@ contract SimpleStaking is
     PausableUpgradeable
 {
     //implement your code here to use SafeERC20Upgradeable and AddressUpgradeable
+    IERC20Upgradeable private rwToken;
 
     address public rwTokenAddr; //reward token address
     uint256 public rewardInterval;
+
+    uint256 private _totalSupply;
 
     struct Record {
         uint256 stakedAmount;
@@ -23,8 +27,10 @@ contract SimpleStaking is
     }
 
     //implement your code here for "records", a mapping of token addresses and user addresses to an user Record struct
+    mapping(address => mapping(address => Record)) private records;
 
     //implement your code here for "rewardRates", a mapping of token address to reward rates. e.g. if APY is 20%, then rewardRate is 20.
+    mapping(address => uint256) public rewardRates;
 
     event Stake(address indexed user, uint256 amount, uint256 stakedAt);
     event Unstake(
@@ -48,24 +54,33 @@ contract SimpleStaking is
 
     function initialize(address _rwTokenAddr) external initializer {
         //implement your code here
+        rwTokenAddr = _rwTokenAddr;
+        rwToken = IERC20Upgradeable(_rwTokenAddr);
     }
 
     // for users to stake tokens
     function stake(address tokenAddr, uint256 amount) external whenNotPaused {
         //implement your code here
+        _totalSupply += amount;
+        records[tokenAddr][msg.sender].stakedAmount += amount;
+        records[tokenAddr][msg.sender].stakedAt = block.timestamp;
+
+        rwToken.transferFrom(msg.sender, address(this), amount);
     }
 
     //for users to unstake their staked tokens
     function unstake(address tokenAddr, uint256 amount) external whenNotPaused {
         //implement your code here
+        _totalSupply -= amount;
+        records[tokenAddr][msg.sender].stakedAmount -= amount;
+        records[tokenAddr][msg.sender].unstakedAt = block.timestamp;
+        records[tokenAddr][msg.sender].unstakedAmount += amount;
     }
 
     //for users to withdraw their unstaked tokens from this contract to the caller's address
-    function withdrawUnstaked(address tokenAddr, uint256 _amount)
-        external
-        whenNotPaused
-    {
+    function withdrawUnstaked(uint256 _amount) external whenNotPaused {
         //implement your code here
+        rwToken.transfer(msg.sender, _amount);
     }
 
     //for users to withdraw reward tokens from this contract to the caller's address
